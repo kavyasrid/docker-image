4f7fa3d8d33d6fe515ab349f8295aed413975ae8
kavyasrid <53416878+kavyasrid@users.noreply.github.com>
2019-07-29 23:49:57 -0400

    Add files via upload

---
+++ gs-accessing-data-jpa/src/main/java/org/springframework/gs_accessing_data_jpa/App.java
@@ -0,0 +1,13 @@
+package org.springframework.gs_accessing_data_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
